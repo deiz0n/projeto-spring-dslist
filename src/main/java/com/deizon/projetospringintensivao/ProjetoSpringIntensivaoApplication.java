946d51667bb6e9f4a208fad09aabf539fde880d3
deizon <eduardomacedosilva@outlook.com>
2023-05-09 21:58:35 -0300

    Criação do projeto

---
+++ src/main/java/com/deizon/projetospringintensivao/ProjetoSpringIntensivaoApplication.java
@@ -0,0 +1,13 @@
+package com.deizon.projetospringintensivao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringIntensivaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoSpringIntensivaoApplication.class, args);
+    }
+
+}
