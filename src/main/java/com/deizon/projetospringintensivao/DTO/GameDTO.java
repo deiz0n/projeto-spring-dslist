191df64c5bafec1c5eeb8e3bc8c1771e6e3d3f68
deizon <eduardomacedosilva@outlook.com>
2023-05-10 23:51:15 -0300

    Criação do GameDTO

---
+++ src/main/java/com/deizon/projetospringintensivao/DTO/GameDTO.java
@@ -0,0 +1,25 @@
+package com.deizon.projetospringintensivao.DTO;
+
+import com.deizon.projetospringintensivao.entities.Game;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class GameDTO {
+
+    private Long id;
+    private String title;
+    private Integer year;
+    private String imgUrl;
+    private String shortDescription;
+
+    public GameDTO(Game entity) {
+        id = entity.getId();
+        title = entity.getTitle();
+        year = entity.getYear();
+        imgUrl = entity.getImgUrl();
+        shortDescription = entity.getShortDescription();
+    }
+}
